38be5f91719087e0025470b5ddb8d9cc17c5c36c
Alex Bea <abea@mailbox.org>
2021-02-01 14:22:28 -0600

    Removes apos-page subnav

--- src/templates/apos-page.js
+++ src/templates/apos-page.js
@@ -5,14 +5,6 @@ import Layout from "../components/Layout"
 export default function AposPage({ data }) {
   const page = data.aposCorePage
   let subNav = []
-  if (data.allSitePage && data.allSitePage.edges) {
-    subNav = data.allSitePage.edges.map(e => {
-      return {
-        title: e.node.context.title,
-        slug: e.node.context.slug,
-      }
-    })
-  }
 
   return (
     <Layout>
@@ -22,18 +14,6 @@ export default function AposPage({ data }) {
           {!!subNav.length && (
             <nav className="mb-6">
               <ul>
-                {subNav.map(item => {
-                  return (
-                    <li className="mr-6" key={item.slug}>
-                      <a
-                        className="text-blue-500 hover:text-blue-800"
-                        href={item.slug}
-                      >
-                        {item.title}
-                      </a>
-                    </li>
-                  )
-                })}
               </ul>
             </nav>
           )}
